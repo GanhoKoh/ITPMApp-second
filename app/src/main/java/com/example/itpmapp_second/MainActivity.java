e18441e8ca93a9e65a85d6ee84be1a3a23e780f4
Ganho Kou <publickohcz@gmail.com>
2019-12-31 11:01:35 +0900

    エラーになったので、そこまでコミット

--- app/src/main/java/com/example/itpmapp_second/MainActivity.java
+++ app/src/main/java/com/example/itpmapp_second/MainActivity.java
@@ -21,5 +21,4 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
-
 }
--- app/src/main/java/com/example/itpmapp_second/SplashActivity.java
+++ app/src/main/java/com/example/itpmapp_second/SplashActivity.java
@@ -2,13 +2,19 @@ package com.example.itpmapp_second;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class
+        ));
     }
 }
