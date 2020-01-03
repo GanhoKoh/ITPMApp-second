5735084f4dfc0a850e00f48a9fb4bc719a2b6d2d
Ganho Kou <publickohcz@gmail.com>
2020-01-03 19:56:51 +0900

    リスト表示を追加。高度な書き方だった

--- app/src/main/java/com/example/itpmapp_second/MainActivity.java
+++ app/src/main/java/com/example/itpmapp_second/MainActivity.java
@@ -9,11 +9,23 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private ArrayAdapter<String> mAdapter;
+    //"ホーム","事業内容","企業情報","採用情報","お問い合わせ"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.mainToolBar);
         setSupportActionBar(toolbar);
+
+        mListView = findViewById(R.id.main_list);
+
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+
+        mListView.setAdapter(mAdapter);
+        //通常クリックのリスナー
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(MainActivity.class.getSimpleName(), position + "項目目のリストアイテムが押されました");
+                String title = String.valueOf(parent.getItemAtPosition(position));
+                Intent intent = EditActivity.createIntent(MainActivity.this, title);
+                startActivity(intent);
+            }
+        });
+        //長押しクリックのリスナー
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(MainActivity.class.getSimpleName(), position + "項目目のリストアイテムが長押しされました");
+                String  title = String.valueOf(parent.getItemAtPosition(position));
+                mAdapter.remove(title);
+                return true;
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +68,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        displayDataList();
+    }
+
+    private void displayDataList() {
+        mAdapter.clear();
+
+        List<String> dataList = Arrays.asList("ホーム","事業内容","企業情報","採用情報","お問い合わせ");
+        mAdapter.addAll(dataList);
+
+        mAdapter.notifyDataSetChanged();
     }
 }
